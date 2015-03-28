f0a3bf55bdd05654980a696623caf52ee2bd319a
AB <alyseb@users.noreply.github.com>
2015-03-28 16:02:05 -0700

    Created bio added CIMMLogo.jpg

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,11 +1,43 @@
-var awesomeThoughts = "I am AB and I am AWESOME!";
-
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-
-var formattedName = HTMLheaderName.replace("%data%", "AB");
+var formattedName = HTMLheaderName.replace("%data%","AB");
 
 var role = "Web Developer";
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+
+var bio = {
+	"name" : "AB",
+	"role" : "Web Developer",
+	"contacts" : {
+		"mobile" : "1-555-555-5555",
+		"email" : "@gmail",
+		"github" : "ab",
+		"twitter" : "@ab",
+		"location" : "town"
+	},
+	"welcomeMsg" : "lorem ipsum dolor sit amet etc etc etc.",
+
+	"skills":[
+		"web development",
+		"HTML",
+		"CSS",
+		"JavaScript",
+		"jQuery",
+		"Git",
+		"GitHub",
+		"Wordpress",
+		"Simply Accounting",
+		"technology set up and maintenance",
+		"data entry",
+		"tech training",
+		"template creation",
+		"Excel Spreadsheet",
+		"Powerpoint"
+			],
+		
+	"bioPic" : "images/fry.jpg"
+}
+
+
